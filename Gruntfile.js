6183f8b6166e94b64914771339af60c6e3db57ef
Ethan Smith <ethan@onebytegone.com>
2016-01-03 19:26:46 -0500

    Basic grunt setup

---
+++ Gruntfile.js
@@ -0,0 +1,61 @@
+module.exports = function(grunt) {
+   grunt.loadNpmTasks('grunt-contrib-watch');
+   grunt.loadNpmTasks('grunt-browserify');
+   grunt.loadNpmTasks('grunt-exorcise');
+   grunt.loadNpmTasks('grunt-contrib-copy');
+
+   grunt.initConfig({
+      project: {
+         src: {
+            root: './src',
+            js: '<%= project.src.root %>/js'
+         },
+         dist: {
+            root: 'dist',
+            js: '<%= project.dist.root %>/js'
+         }
+      },
+      browserify: {
+         js: {
+            options: {
+               browserifyOptions: {
+                  debug: true
+               }
+            },
+            files: {
+              '<%= project.dist.js %>/bundle.js': '<%= project.src.root %>/app.js'
+            }
+         }
+      },
+      exorcise: {
+         bundle: {
+            options: {},
+            files: {
+               '<%= project.dist.js %>/bundle.map': '<%= project.dist.js %>/bundle.js'
+            }
+         }
+      },
+      copy: {
+         app: {
+            files: [
+               {
+                  expand: true,
+                  cwd: '<%= project.src.root %>',
+                  src: 'index.html',
+                  dest: '<%= project.dist.root %>',
+                  filter: 'isFile'
+               },
+            ]
+         }
+      },
+      watch: {
+         files: [
+            '<%= project.src.root %>/**/*.js',
+            '<%= project.src.root %>/**/*.html',
+         ],
+         tasks: ['default']
+      }
+   });
+
+   grunt.registerTask('default', ['browserify', 'exorcise', 'copy:app']);
+};
