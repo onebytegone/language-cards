6366a3235d849aac371061535943f1a7a7d73883
Ethan Smith <ethan@onebytegone.com>
2016-01-20 08:00:37 -0500

    Add empty view

--- src/js/view/WordList.js
+++ src/js/view/WordList.js
@@ -1,8 +1,14 @@
 var Marionette = require('backbone.marionette'),
     WordListItem = require('./WordListItem');
 
+var NoChildrenView = Marionette.ItemView.extend({
+   tagName: 'li',
+   template: "#template-no-words-positive"
+});
+
 module.exports = Marionette.CollectionView.extend({
    tagName: 'ul',
    className: 'wordlist',
-   childView: WordListItem
+   childView: WordListItem,
+   emptyView: NoChildrenView
 });
